21e2c437ca076dc62ad90224d04f79abeb875ac4
AmpereProcessos <ampereprocessos@gmail.com>
2024-12-02 18:00:36 -0300

    update in OpportunityClient styles

--- components/Opportunities/OpportunityClient.tsx
+++ components/Opportunities/OpportunityClient.tsx
@@ -1,9 +1,9 @@
-import { formatDateAsLocale } from '@/lib/methods/formatting'
+import { formatDateAsLocale, formatLocation } from '@/lib/methods/formatting'
 import { TClientDTO } from '@/utils/schemas/client.schema'
 import React, { useState } from 'react'
 import { AiFillEdit, AiOutlineUser } from 'react-icons/ai'
 import { BsCalendarPlus, BsTelephoneFill } from 'react-icons/bs'
-import { FaCity, FaUser } from 'react-icons/fa'
+import { FaCity, FaRing, FaUser } from 'react-icons/fa'
 import { GiPositionMarker } from 'react-icons/gi'
 import { HiIdentification } from 'react-icons/hi'
 import { MdEmail } from 'react-icons/md'
@@ -11,6 +11,8 @@ import Avatar from '../utils/Avatar'
 import { Session } from 'next-auth'
 import EditClient from '../Modals/Client/EditClient'
 import { TOpportunity } from '@/utils/schemas/opportunity.schema'
+import { Accessibility, BriefcaseBusiness, Building2, Cake, Filter, Phone, User, UserRound } from 'lucide-react'
+import { FaLocationDot, FaRegIdCard } from 'react-icons/fa6'
 
 type OpportunityClientProps = {
   opportunityId: string
@@ -32,8 +34,135 @@ function OpportunityClient({ opportunityId, client, responsibles, session }: Opp
           </button>
         ) : null}
       </div>
-      <div className="mt-3 flex w-full grow flex-col gap-1 lg:flex-row">
-        <div className="flex h-full w-full flex-col items-start justify-around gap-2 lg:w-[50%] lg:items-center">
+
+      <div className="mt-3 flex w-full grow flex-col gap-2">
+        <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
+          <div className="flex flex-col items-center gap-1 lg:items-start">
+            <p className="text-[0.65rem] font-medium text-gray-500">GERAIS</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <UserRound size={15} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.nome}</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaRegIdCard size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.cpfCnpj}</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <Cake size={15} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">
+                  {client.dataNascimento ? formatDateAsLocale(client.dataNascimento, true) : 'NÃO DEFINIDO'}
+                </p>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaRing size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.estadoCivil || 'NÃO DEFINIDO'}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
+          <div className="flex flex-col items-center gap-1 lg:items-start">
+            <p className="text-[0.65rem] font-medium text-gray-500">CONTATOS</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <Phone size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.telefonePrimario}</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <MdEmail size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.email || 'NÃO DEFINIDO'}</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-1 lg:items-end">
+            <p className="text-[0.65rem] font-medium text-gray-500">AQUISIÇÃO</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <Filter size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.canalAquisicao}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
+          <div className="flex flex-col items-center gap-1 lg:items-start">
+            <p className="text-[0.65rem] font-medium text-gray-500">LOCALIZAÇÃO</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <FaLocationDot size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">
+                  {formatLocation({
+                    location: {
+                      cep: client.cep,
+                      uf: client.uf,
+                      cidade: client.cidade,
+                      bairro: client.bairro,
+                      endereco: client.endereco,
+                      numeroOuIdentificador: client.numeroOuIdentificador,
+                      complemento: client.complemento,
+                    },
+                    includeCity: true,
+                    includeUf: true,
+                  })}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-1 lg:items-end">
+            <p className="text-[0.65rem] font-medium text-gray-500">ESTADO CIVIL</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <FaRing size={15} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.canalAquisicao}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
+          <div className="flex flex-col items-center gap-1 lg:items-start">
+            <p className="text-[0.65rem] font-medium text-gray-500">TRABALHO</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <BriefcaseBusiness size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.profissao || 'NÃO DEFINIDO'}</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <Building2 size={12} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.ondeTrabalha || 'NÃO DEFINIDO'}</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-1 lg:items-end">
+            <p className="text-[0.65rem] font-medium text-gray-500">DEFICIÊNCIA</p>
+            <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-start">
+              <div className="flex items-center gap-1">
+                <Accessibility size={15} />
+                <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.deficiencia || 'NÃO DEFINIDO'}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <p className="text-[0.65rem] font-medium text-gray-500">INDICAÇÃO</p>
+          <div className="flex flex-wrap items-center justify-center gap-2 lg:justify-center">
+            {client.indicador.nome ? (
+              <>
+                <div className="flex items-center gap-1">
+                  <User size={15} />
+                  <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.indicador.nome}</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <Phone size={15} />
+                  <p className="text-[0.75rem] font-medium leading-none tracking-tight">{client.indicador.contato || 'NÃO DEFINIDO'}</p>
+                </div>
+              </>
+            ) : (
+              <p className="text-[0.75rem] font-medium leading-none tracking-tight">NÃO APLICÁVEL</p>
+            )}
+          </div>
+        </div>
+        {/* <div className="flex h-full w-full flex-col items-start justify-around gap-2 lg:w-[50%] lg:items-center">
           <div className="flex w-full items-start justify-center gap-2 lg:justify-start">
             <div className="flex h-[25px] min-h-[25px] w-[25px] min-w-[25px] items-center justify-center rounded-full border border-black p-1">
               <FaUser size={12} />
@@ -76,7 +205,7 @@ function OpportunityClient({ opportunityId, client, responsibles, session }: Opp
               {client.endereco || 'NÃO DEFINIDO'}, {client.bairro || 'NÃO DEFINIDO'}, Nº {client.numeroOuIdentificador}, CEP: {client.cep || 'NÃO DEFINIDO'}
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="flex w-full items-center justify-end gap-2">
         <div className={`flex items-center gap-1`}>
