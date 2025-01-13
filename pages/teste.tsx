2df8937ccc1a089bdc904cf7af9e4759c877c2fa
AmpereProcessos <ampereprocessos@gmail.com>
2025-01-13 15:22:15 -0300

    fix to teste page import

--- pages/teste.tsx
+++ pages/teste.tsx
@@ -7,7 +7,6 @@ import axios from 'axios'
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
-import OpportunitiesSendAndReceived from '@/opportunities-send-received-2024.json'
 function Testing() {
   const { data: session, status } = useSession()
 
@@ -16,7 +15,6 @@ function Testing() {
   return (
     <div className="flex h-full flex-col md:flex-row">
       <Sidebar session={session} />
-      <div className="flex w-full max-w-full grow flex-col overflow-x-hidden bg-[#f8f9fa] p-6">{OpportunitiesSendAndReceived['ENVIOS']}</div>
     </div>
   )
 }
