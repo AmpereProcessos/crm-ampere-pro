e45d2f26c3b80fbc38b000aa02d1df915a3519d5
AmpereProcessos <ampereprocessos@gmail.com>
2025-04-17 10:47:22 -0300

    add fix to opportunities query options

--- pages/api/opportunities/query-options.ts
+++ pages/api/opportunities/query-options.ts
@@ -1,51 +1,51 @@
-import { getPartnerFunnels } from '@/repositories/funnels/queries'
-import { getPartnersSimplified } from '@/repositories/partner-simplified/query'
-import { getProjectTypesSimplified } from '@/repositories/project-type/queries'
-import { getOpportunityCreators } from '@/repositories/users/queries'
-import connectToDatabase from '@/services/mongodb/crm-db-connection'
-import { apiHandler, validateAuthenticationWithSession } from '@/utils/api'
-import { TFunnel, TFunnelDTO } from '@/utils/schemas/funnel.schema'
-import { TPartner, TPartnerSimplifiedDTO } from '@/utils/schemas/partner.schema'
-import { TProjectType, TProjectTypeDTOSimplified } from '@/utils/schemas/project-types.schema'
-import { TUser, TUserDTOSimplified } from '@/utils/schemas/user.schema'
-import { Collection, Filter } from 'mongodb'
-import { NextApiHandler } from 'next'
+import { getPartnerFunnels } from "@/repositories/funnels/queries";
+import { getPartnersSimplified } from "@/repositories/partner-simplified/query";
+import { getProjectTypesSimplified } from "@/repositories/project-type/queries";
+import { getOpportunityCreators } from "@/repositories/users/queries";
+import connectToDatabase from "@/services/mongodb/crm-db-connection";
+import { apiHandler, validateAuthenticationWithSession } from "@/utils/api";
+import type { TFunnel, TFunnelDTO } from "@/utils/schemas/funnel.schema";
+import type { TPartner, TPartnerSimplifiedDTO } from "@/utils/schemas/partner.schema";
+import type { TProjectType, TProjectTypeDTOSimplified } from "@/utils/schemas/project-types.schema";
+import type { TUser, TUserDTOSimplified } from "@/utils/schemas/user.schema";
+import type { Collection, Filter } from "mongodb";
+import type { NextApiHandler } from "next";
 
 export type TOpportunitiesQueryOptions = {
-  responsibles: TUserDTOSimplified[]
-  partners: TPartnerSimplifiedDTO[]
-  projectTypes: TProjectTypeDTOSimplified[]
-  funnels: TFunnelDTO[]
-}
+	responsibles: TUserDTOSimplified[];
+	partners: TPartnerSimplifiedDTO[];
+	projectTypes: TProjectTypeDTOSimplified[];
+	funnels: TFunnelDTO[];
+};
 type GetResponse = {
-  data: TOpportunitiesQueryOptions
-}
+	data: TOpportunitiesQueryOptions;
+};
 
 const getOpportunitiesQueryOptions: NextApiHandler<GetResponse> = async (req, res) => {
-  const session = await validateAuthenticationWithSession(req, res)
-  const partnerScope = session.user.permissoes.parceiros.escopo
+	const session = await validateAuthenticationWithSession(req, res);
+	const partnerScope = session.user.permissoes.parceiros.escopo;
 
-  const partnerQuery = partnerScope ? { idParceiro: { $in: [...partnerScope, null] } } : {}
+	const partnerQuery = partnerScope ? { idParceiro: { $in: [...partnerScope, null] } } : {};
 
-  const db = await connectToDatabase(process.env.MONGODB_URI, 'crm')
-  const usersCollection: Collection<TUser> = db.collection('users')
-  const partnersCollection: Collection<TPartner> = db.collection('partners')
-  const projectTypesCollection: Collection<TProjectType> = db.collection('project-types')
-  const funnelsCollection: Collection<TFunnel> = db.collection('funnels')
+	const db = await connectToDatabase();
+	const usersCollection: Collection<TUser> = db.collection("users");
+	const partnersCollection: Collection<TPartner> = db.collection("partners");
+	const projectTypesCollection: Collection<TProjectType> = db.collection("project-types");
+	const funnelsCollection: Collection<TFunnel> = db.collection("funnels");
 
-  const responsibles = await getOpportunityCreators({ collection: usersCollection, query: partnerQuery as Filter<TUser> })
-  const partners = await getPartnersSimplified({ collection: partnersCollection, query: partnerQuery as Filter<TPartner> })
-  const projectTypes = await getProjectTypesSimplified({ collection: projectTypesCollection, query: partnerQuery as Filter<TProjectType> })
-  const funnels = await getPartnerFunnels({ collection: funnelsCollection, query: partnerQuery as Filter<TFunnel> })
+	const responsibles = await getOpportunityCreators({ collection: usersCollection, query: partnerQuery as Filter<TUser> });
+	const partners = await getPartnersSimplified({ collection: partnersCollection, query: partnerQuery as Filter<TPartner> });
+	const projectTypes = await getProjectTypesSimplified({ collection: projectTypesCollection, query: partnerQuery as Filter<TProjectType> });
+	const funnels = await getPartnerFunnels({ collection: funnelsCollection, query: partnerQuery as Filter<TFunnel> });
 
-  const options = {
-    responsibles: responsibles.map((r) => ({ ...r, _id: r._id.toString() })),
-    partners: partners.map((p) => ({ ...p, _id: p._id.toString() })),
-    projectTypes: projectTypes.map((p) => ({ ...p, _id: p._id.toString() })),
-    funnels: funnels.map((f) => ({ ...f, _id: f._id.toString() })),
-  }
+	const options = {
+		responsibles: responsibles.map((r) => ({ ...r, _id: r._id.toString() })),
+		partners: partners.map((p) => ({ ...p, _id: p._id.toString() })),
+		projectTypes: projectTypes.map((p) => ({ ...p, _id: p._id.toString() })),
+		funnels: funnels.map((f) => ({ ...f, _id: f._id.toString() })),
+	};
 
-  return res.status(200).json({ data: options })
-}
+	return res.status(200).json({ data: options });
+};
 
-export default apiHandler({ GET: getOpportunitiesQueryOptions })
+export default apiHandler({ GET: getOpportunitiesQueryOptions });
--- repositories/users/queries.ts
+++ repositories/users/queries.ts
@@ -1,134 +1,140 @@
-import { TUser, TUserDTOWithSaleGoals, TUserEntity, TUserSimplified, simplifiedProjection } from '@/utils/schemas/user.schema'
-import { Collection, Filter, ObjectId, WithId } from 'mongodb'
-import { Session } from 'next-auth'
+import { type TUser, type TUserDTOWithSaleGoals, type TUserEntity, type TUserSimplified, simplifiedProjection } from "@/utils/schemas/user.schema";
+import { type Collection, type Filter, ObjectId, type WithId } from "mongodb";
 
 type GetAllUsers = {
-  collection: Collection<TUserEntity>
-}
+	collection: Collection<TUserEntity>;
+};
 export async function getAllUsers({ collection }: GetAllUsers) {
-  try {
-    const users = await collection.find({}, { projection: { senha: 0 }, sort: { nome: 1 } }).toArray()
-    return users
-  } catch (error) {
-    throw error
-  }
+	try {
+		const users = await collection.find({}, { projection: { senha: 0 }, sort: { nome: 1 } }).toArray();
+		return users;
+	} catch (error) {
+		console.log("Error fetching all users:", error);
+		throw error;
+	}
 }
 type GetPartnerUsers = {
-  collection: Collection<TUserEntity>
-  query: Filter<TUser>
-}
+	collection: Collection<TUserEntity>;
+	query: Filter<TUser>;
+};
 export async function getPartnerUsers({ collection, query }: GetPartnerUsers) {
-  try {
-    const users = await collection.find({ ...query }, { projection: { senha: 0 }, sort: { nome: 1 } }).toArray()
-    return users
-  } catch (error) {
-    throw error
-  }
+	try {
+		const users = await collection.find({ ...query }, { projection: { senha: 0 }, sort: { nome: 1 } }).toArray();
+		return users;
+	} catch (error) {
+		console.log("Error fetching partner users:", error);
+		throw error;
+	}
 }
 type GetUserById = {
-  collection: Collection<TUserEntity>
-  id: string
-  query: Filter<TUser>
-}
+	collection: Collection<TUserEntity>;
+	id: string;
+	query: Filter<TUser>;
+};
 export async function getUserById({ collection, id, query }: GetUserById) {
-  try {
-    const user = await collection.findOne({ _id: new ObjectId(id), ...query }, { projection: { senha: 0 } })
-    return user
-  } catch (error) {
-    throw error
-  }
+	try {
+		const user = await collection.findOne({ _id: new ObjectId(id), ...query }, { projection: { senha: 0 } });
+		return user;
+	} catch (error) {
+		console.log("Error fetching user by ID:", error);
+		throw error;
+	}
 }
 type GetOpportunityCreators = {
-  collection: Collection<TUser>
-  query: Filter<TUser>
-}
+	collection: Collection<TUser>;
+	query: Filter<TUser>;
+};
 export async function getOpportunityCreators({ collection, query }: GetOpportunityCreators) {
-  try {
-    const creators = await collection
-      .find({ 'permissoes.oportunidades.criar': true, ...query }, { sort: { nome: 1 } })
-      .project(simplifiedProjection)
-      .toArray()
-    return creators as WithId<TUserSimplified>[]
-  } catch (error) {
-    throw error
-  }
+	try {
+		const creators = await collection
+			.find({ "permissoes.oportunidades.criar": true, ...query, ativo: true, dataExclusao: null }, { sort: { nome: 1 } })
+			.project(simplifiedProjection)
+			.toArray();
+		return creators as WithId<TUserSimplified>[];
+	} catch (error) {
+		console.log("Error fetching opportunity creators:", error);
+		throw error;
+	}
 }
 type GetTechnicalAnalystsParams = {
-  collection: Collection<TUser>
-  query: Filter<TUser>
-}
+	collection: Collection<TUser>;
+	query: Filter<TUser>;
+};
 
 export async function getTechnicalAnalysts({ collection, query }: GetTechnicalAnalystsParams) {
-  try {
-    const analysts = await collection
-      .find({ 'permissoes.analisesTecnicas.editar': true, ...query }, { sort: { nome: 1 } })
-      .project(simplifiedProjection)
-      .toArray()
-    return analysts as WithId<TUser>[]
-  } catch (error) {
-    throw error
-  }
+	try {
+		const analysts = await collection
+			.find({ "permissoes.analisesTecnicas.editar": true, ...query }, { sort: { nome: 1 } })
+			.project(simplifiedProjection)
+			.toArray();
+		return analysts as WithId<TUser>[];
+	} catch (error) {
+		console.log("Error fetching technical analysts:", error);
+		throw error;
+	}
 }
 
 type GetLeadReceiversParams = {
-  collection: Collection<TUser>
-  query: Filter<TUser>
-}
+	collection: Collection<TUser>;
+	query: Filter<TUser>;
+};
 export async function getLeadReceivers({ collection, query }: GetLeadReceiversParams) {
-  try {
-    const receivers = await collection
-      .find({ 'permissoes.integracoes.receberLeads': true, ...query }, { sort: { nome: 1 } })
-      .project(simplifiedProjection)
-      .toArray()
-    return receivers as WithId<TUser>[]
-  } catch (error) {
-    throw error
-  }
+	try {
+		const receivers = await collection
+			.find({ "permissoes.integracoes.receberLeads": true, ...query }, { sort: { nome: 1 } })
+			.project(simplifiedProjection)
+			.toArray();
+		return receivers as WithId<TUser>[];
+	} catch (error) {
+		console.log("Error fetching lead receivers:", error);
+		throw error;
+	}
 }
 
 type GetSalePromotersParams = {
-  collection: Collection<TUser>
-  query: Filter<TUser>
-}
+	collection: Collection<TUser>;
+	query: Filter<TUser>;
+};
 
 export async function getSalePromoters({ collection, query }: GetSalePromotersParams) {
-  try {
-    const salePromoters = await collection
-      .aggregate([
-        {
-          $match: {
-            ativo: true,
-            idGrupo: { $in: ['66562a2a812707dbf9f04832', '66562a2a812707dbf9f04833'] },
-            ...query,
-          },
-        },
-        {
-          $project: {
-            senha: 0,
-          },
-        },
-        {
-          $addFields: {
-            userIdString: { $toString: '$_id' },
-          },
-        },
-        {
-          $lookup: {
-            from: 'sale-goals',
-            localField: 'userIdString',
-            foreignField: 'usuario.id',
-            as: 'metas',
-          },
-        },
-        {
-          $sort: {
-            nome: 1,
-          },
-        },
-      ])
-      .toArray()
-    return salePromoters as TUserDTOWithSaleGoals[]
-  } catch (error) {
-    throw error
-  }
+	try {
+		const salePromoters = await collection
+			.aggregate([
+				{
+					$match: {
+						ativo: true,
+						idGrupo: { $in: ["66562a2a812707dbf9f04832", "66562a2a812707dbf9f04833"] },
+						...query,
+					},
+				},
+				{
+					$project: {
+						senha: 0,
+					},
+				},
+				{
+					$addFields: {
+						userIdString: { $toString: "$_id" },
+					},
+				},
+				{
+					$lookup: {
+						from: "sale-goals",
+						localField: "userIdString",
+						foreignField: "usuario.id",
+						as: "metas",
+					},
+				},
+				{
+					$sort: {
+						nome: 1,
+					},
+				},
+			])
+			.toArray();
+		return salePromoters as TUserDTOWithSaleGoals[];
+	} catch (error) {
+		console.log("Error fetching sale promoters:", error);
+		throw error;
+	}
 }
