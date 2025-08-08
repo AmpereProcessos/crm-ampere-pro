b391ea6a44413852dd6f5cc3745d0f52e40ebba0
AmpereProcessos <ampereprocessos@gmail.com>
2025-08-08 16:14:08 -0300

    Integrate general notification workflow into the API route

--- app/api/novu/route.ts
+++ app/api/novu/route.ts
@@ -1,6 +1,11 @@
 import { serve } from "@novu/framework/next";
-import { notfiyNewOpportunityToResponsibles, notifyNewInteractionToResponsibles, notifyOpportunityTopicOnTechnicalAnalysisConcluded } from "@/services/novu/workflows";
+import {
+	generalNoficationWorkflow,
+	notfiyNewOpportunityToResponsibles,
+	notifyNewInteractionToResponsibles,
+	notifyOpportunityTopicOnTechnicalAnalysisConcluded,
+} from "@/services/novu/workflows";
 
 export const { GET, POST, OPTIONS } = serve({
-	workflows: [notfiyNewOpportunityToResponsibles, notifyNewInteractionToResponsibles, notifyOpportunityTopicOnTechnicalAnalysisConcluded],
+	workflows: [notfiyNewOpportunityToResponsibles, notifyNewInteractionToResponsibles, notifyOpportunityTopicOnTechnicalAnalysisConcluded, generalNoficationWorkflow],
 });
