2e5dfe2ce51ffad4241559c9d5cbc2465705adf1
AmpereProcessos <ampereprocessos@gmail.com>
2025-10-14 16:01:12 -0300

    add better validation for client phone number filling

--- repositories/clients/mutations.ts
+++ repositories/clients/mutations.ts
@@ -1,31 +1,41 @@
-import { TClient } from '@/utils/schemas/client.schema'
-import { Collection, Filter, ObjectId } from 'mongodb'
+import { formatPhoneAsBase } from "@/utils/methods";
+import type { TClient } from "@/utils/schemas/client.schema";
+import { type Collection, type Filter, ObjectId } from "mongodb";
 
 type InsertClientParams = {
-  collection: Collection<TClient>
-  info: TClient
-  partnerId: string
-}
+	collection: Collection<TClient>;
+	info: TClient;
+	partnerId: string;
+};
 
-export async function insertClient({ collection, info, partnerId }: InsertClientParams) {
-  try {
-    const insertResponse = await collection.insertOne({ ...info, idParceiro: partnerId, dataInsercao: new Date().toISOString() })
-    return insertResponse
-  } catch (error) {
-    throw error
-  }
+export async function insertClient({
+	collection,
+	info,
+	partnerId,
+}: InsertClientParams) {
+	const insertResponse = await collection.insertOne({
+		...info,
+		telefonePrimarioBase: formatPhoneAsBase(info.telefonePrimario ?? ""),
+		idParceiro: partnerId,
+		dataInsercao: new Date().toISOString(),
+	});
+	return insertResponse;
 }
 type UpdateClientParams = {
-  id: string
-  collection: Collection<TClient>
-  changes: any
-  query: Filter<TClient>
-}
-export async function updateClient({ id, collection, changes, query }: UpdateClientParams) {
-  try {
-    const updateResponse = await collection.updateOne({ _id: new ObjectId(id), ...query }, { $set: { ...changes } })
-    return updateResponse
-  } catch (error) {
-    throw error
-  }
+	id: string;
+	collection: Collection<TClient>;
+	changes: Partial<TClient>;
+	query: Filter<TClient>;
+};
+export async function updateClient({
+	id,
+	collection,
+	changes,
+	query,
+}: UpdateClientParams) {
+	const updateResponse = await collection.updateOne(
+		{ _id: new ObjectId(id), ...query },
+		{ $set: { ...changes } },
+	);
+	return updateResponse;
 }
