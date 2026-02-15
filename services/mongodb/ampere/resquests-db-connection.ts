cf8b32b7d472d7f0dd0a210c1c89659f5278da3c
Lucas Fernandes <ampereprocessos@gmail.com>
2026-02-15 11:47:23 -0300

    feat: Enhance PaymentInfo component with payer data management and address lookup
    - Introduced functions to update payer data and location within the PaymentInfo component, improving data handling.
    - Added address lookup functionality using CEP, integrating toast notifications for user feedback.
    - Updated the contract request schema to include detailed payer information, ensuring comprehensive data capture.
    - Refactored input fields to bind directly to the new payer structure, enhancing user experience and data integrity.

--- services/mongodb/ampere/resquests-db-connection.ts
+++ services/mongodb/ampere/resquests-db-connection.ts
@@ -1,12 +1,11 @@
 import createHttpError from "http-errors";
-import { Db, MongoClient } from "mongodb";
+import { type Db, MongoClient } from "mongodb";
 
 let cachedDb: Db | null = null;
-export default async function connectToAmpereDatabase(uri: unknown) {
-	if (typeof uri != "string") throw createHttpError.InternalServerError("Wrong databse URI.");
-	if (cachedDb) {
-		return cachedDb;
-	}
+export default async function connectToAmpereDatabase() {
+	const uri = process.env.MONGODB_URI;
+	if (!uri) throw createHttpError.InternalServerError("Wrong databse URI.");
+	if (cachedDb) return cachedDb;
 	const client = await MongoClient.connect(uri);
 	const db = client.db("solicitacoes");
 	cachedDb = db;
