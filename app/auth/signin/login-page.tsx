7ee54ee16bd3514cf43ee97bb4aadac2cc558862
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2026-06-10 13:39:34 -0300

    feat: refactor login functionality and improve error handling
    - Updated the login component to utilize the new action state management for handling login actions.
    - Enhanced error handling by replacing search parameters with action result feedback for form errors and field-specific errors.
    - Simplified the login page by removing unnecessary props and improving the overall structure for better readability.
    - Adjusted the login function to return structured error messages instead of redirects, streamlining the user experience during authentication.

--- app/auth/signin/login-page.tsx
+++ app/auth/signin/login-page.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import FullScreenWrapper from "@/components/Wrappers/FullScreenWrapper";
 import { login } from "@/lib/auth/actions";
 import { cn } from "@/lib/utils";
-
 import AmpereWhiteLogo from "@/utils/svgs/blue-logo-borders-only.svg";
 import AmpereHorizontalLogo from "@/utils/svgs/horizontal-blue-logo-with-text.svg";
 import Image from "next/image";
-type LoginProps = {
-	searchParams?: {
-		error?: string;
-	};
-};
+import { useActionState } from "react";
 
-function Login({ searchParams }: LoginProps) {
+function Login() {
+	const [actionResult, actionMethod] = useActionState(login, {});
+
+	const fieldErrorMessage =
+		actionResult.fieldError?.email ?? actionResult.fieldError?.password ?? null;
+
 	return (
 		<FullScreenWrapper>
 			<div className="w-full h-full grid lg:grid-cols-2">
@@ -26,7 +28,7 @@ function Login({ searchParams }: LoginProps) {
 					</div>
 					<div className="flex flex-1 items-center justify-center">
 						<div className="w-full max-w-xs lg:max-w-md">
-							<form action={login} className={cn("flex flex-col gap-6")}>
+							<form action={actionMethod} className={cn("flex flex-col gap-6")}>
 								<div className="flex flex-col items-center gap-2 text-center">
 									<h1 className="text-2xl font-bold">Acesse sua conta Ampère Energias</h1>
 									<p className="text-muted-foreground text-sm text-balance">Preencha as suas credenciais para acessar ao app.</p>
@@ -49,23 +51,14 @@ function Login({ searchParams }: LoginProps) {
 										</div>
 										<Input id="password" name="password" type="password" placeholder="suasenha123" required className="dark:bg-background" />
 									</div>
-									{searchParams?.error && <p className="text-red-500 w-full text-center">{searchParams.error}</p>}
+									{actionResult.formError ? (
+										<p className="text-red-500 w-full text-center">{actionResult.formError}</p>
+									) : fieldErrorMessage ? (
+										<p className="text-red-500 w-full text-center">{fieldErrorMessage}</p>
+									) : null}
 									<Button type="submit" className="w-full font-bold">
 										Acessar
 									</Button>
-									{/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-										<span className="bg-background text-muted-foreground relative z-10 px-2">Ou continue com</span>
-									</div>
-									<Button variant="outline" className="w-full flex items-center justify-center gap-2 font-medium">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-4 h-4">
-											<title>Login com Google</title>
-											<path
-												d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-												fill="currentColor"
-											/>
-										</svg>
-										Acessar com Google
-									</Button> */}
 								</div>
 							</form>
 						</div>
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,20 +1,13 @@
 export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
-import React from "react";
 import Login from "./login-page";
 import { getCurrentSession } from "@/lib/auth/session";
 
-type LoginPageProps = {
-	searchParams?: Promise<{
-		error?: string;
-	}>;
-};
-
-async function LoginPage({ searchParams }: LoginPageProps) {
-	const awaitedSearchParams = await searchParams;
+async function LoginPage() {
 	const { session, user } = await getCurrentSession();
 	if (session || user) return redirect("/");
-	return <Login searchParams={awaitedSearchParams} />;
+	return <Login />;
 }
 
 export default LoginPage;
